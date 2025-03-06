906976c06c5b224bbf6a11743ba3e829455a2324
Caio A. da Silva <caretakr@proton.me>
2025-03-05 21:12:37 -0300

    Fix: File path

--- src/install.d/40-hardening.sh
+++ src/install.d/40-hardening.sh
@@ -7,6 +7,6 @@ _main() {
 
   (set -ex
     arch-chroot /mnt tee /etc/sysctl.d/90-namespaces.conf \
-      < $(dirname "$0")/assets/etc/sysctl.d/90-namespaces.conf
+      < $(dirname "$0")/../src/assets/etc/sysctl.d/90-namespaces.conf
   ) || exit
 }
